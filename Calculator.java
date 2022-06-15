170c8d1b5a3bf4dca15170c8a62b71e947c0fdb7
Gayathri Mohan <95912046+yamuna1959@users.noreply.github.com>
2022-06-15 18:46:18 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -13,7 +13,9 @@ class Calculator extends Frame implements ActionListener
     TextField t3=new TextField();
 
     Button b1=new Button("Add");
-    Button b2=new Button("Subtract");
+    Button b2=new Button("Sub");
+    Button b3=new Button("Mul");
+    Button b4=new Button("Div");
     Button b5=new Button("Cancel");
 
     Calculator()
@@ -27,7 +29,9 @@ class Calculator extends Frame implements ActionListener
         t3.setBounds(200,180,100,20);
 
         b1.setBounds(50,250,50,20);
-        b2.setBounds(120,250,50,20);
+        b2.setBounds(110,250,50,20);
+        b3.setBounds(170,250,50,20);
+        b4.setBounds(230,250,50,20);
         b5.setBounds(290,250,50,20);
 
         f1.add(l1);
@@ -38,10 +42,14 @@ class Calculator extends Frame implements ActionListener
         f1.add(t3);
         f1.add(b1);
         f1.add(b2);
+        f1.add(b3);
+        f1.add(b4);
         f1.add(b5);
 
         b1.addActionListener(this);
         b2.addActionListener(this);
+        b3.addActionListener(this);
+        b4.addActionListener(this);
         b5.addActionListener(this);
 
         f1.setLayout(null);
@@ -60,6 +68,14 @@ class Calculator extends Frame implements ActionListener
         if(e.getSource()==b2)
         {
             t3.setText(String.valueOf(n1-n2));
+        }
+        if(e.getSource()==b3)
+        {
+            t3.setText(String.valueOf(n1*n2));
+        }
+        if(e.getSource()==b4)
+        {
+            t3.setText(String.valueOf(n1/n2));
         }
         if(e.getSource()==b5)
         {
